f1aa2152005b595bdf469765d34774fddb0c3936
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2012-03-29 14:30:35 +0200

    Convert CMake commands to lower case.

--- CMake/MacroParseArguments.cmake
+++ CMake/MacroParseArguments.cmake
@@ -1,4 +1,4 @@
-# MACRO (MACRO_PARSE_ARGUMENTS prefix arg_names option_names)
+# macro(MACRO_PARSE_ARGUMENTS prefix arg_names option_names)
 #
 #   From http://www.cmake.org/Wiki/CMakeMacroParseArguments:
 #
@@ -29,51 +29,51 @@
 #   MACRO_PARSE_ARGUMENTS also creates a prefix_DEFAULT_ARGS variable containing
 #   the list of all arguments up to the first arg_name encountered.
 
-IF(NOT COMMAND MACRO_PARSE_ARGUMENTS)
+if(NOT COMMAND MACRO_PARSE_ARGUMENTS)
 
-MACRO (MACRO_PARSE_ARGUMENTS prefix arg_names option_names)
+macro(MACRO_PARSE_ARGUMENTS prefix arg_names option_names)
 
-   SET(DEFAULT_ARGS)
+   set(DEFAULT_ARGS)
 
-   FOREACH (arg_name ${arg_names})
-      SET(${prefix}_${arg_name})
-   ENDFOREACH (arg_name)
+   foreach(arg_name ${arg_names})
+      set(${prefix}_${arg_name})
+   endforeach(arg_name)
 
-   FOREACH (option ${option_names})
-      SET(${prefix}_${option} FALSE)
-   ENDFOREACH (option)
+   foreach(option ${option_names})
+      set(${prefix}_${option} FALSE)
+   endforeach(option)
 
-   SET(current_arg_name DEFAULT_ARGS)
-   SET(current_arg_list)
+   set(current_arg_name DEFAULT_ARGS)
+   set(current_arg_list)
 
-   FOREACH (arg ${ARGN})
+   foreach(arg ${ARGN})
 
-      SET(larg_names ${arg_names})
-      LIST(FIND larg_names "${arg}" is_arg_name)
+      set(larg_names ${arg_names})
+      list(FIND larg_names "${arg}" is_arg_name)
 
-      IF (is_arg_name GREATER -1)
+      if(is_arg_name GREATER -1)
 
-         SET(${prefix}_${current_arg_name} ${current_arg_list})
-         SET(current_arg_name "${arg}")
-         SET(current_arg_list)
+         set(${prefix}_${current_arg_name} ${current_arg_list})
+         set(current_arg_name "${arg}")
+         set(current_arg_list)
 
-      ELSE (is_arg_name GREATER -1)
+      else(is_arg_name GREATER -1)
 
-         SET(loption_names ${option_names})
-         LIST(FIND loption_names "${arg}" is_option)
+         set(loption_names ${option_names})
+         list(FIND loption_names "${arg}" is_option)
 
-         IF (is_option GREATER -1)
-            SET(${prefix}_${arg} TRUE)
-         ELSE (is_option GREATER -1)
-            SET(current_arg_list ${current_arg_list} "${arg}")
-         ENDIF (is_option GREATER -1)
+         if(is_option GREATER -1)
+            set(${prefix}_${arg} TRUE)
+         else(is_option GREATER -1)
+            set(current_arg_list ${current_arg_list} "${arg}")
+         endif(is_option GREATER -1)
 
-      ENDIF (is_arg_name GREATER -1)
+      endif(is_arg_name GREATER -1)
 
-   ENDFOREACH (arg ${ARGN})
+   endforeach(arg ${ARGN})
 
-   SET(${prefix}_${current_arg_name} ${current_arg_list})
+   set(${prefix}_${current_arg_name} ${current_arg_list})
 
-ENDMACRO (MACRO_PARSE_ARGUMENTS)
+endmacro(MACRO_PARSE_ARGUMENTS)
 
-ENDIF(NOT COMMAND MACRO_PARSE_ARGUMENTS)
+endif(NOT COMMAND MACRO_PARSE_ARGUMENTS)

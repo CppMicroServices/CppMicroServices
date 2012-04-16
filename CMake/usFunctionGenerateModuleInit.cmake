cf51b26a21cf7871cdcd139639f0bd189e9d2a82
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2012-04-16 12:41:00 +0200

    Documented CMake function.

--- CMake/usFunctionGenerateModuleInit.cmake
+++ CMake/usFunctionGenerateModuleInit.cmake
@@ -1,3 +1,26 @@
+#! Generate a source file which handles proper initialization of a module.
+#!
+#! This CMake function will store the path to a generated source file in the
+#! src_var variable, which should be compiled into a module. Example usage:
+#!
+#! \verbatim
+#! set(module_srcs )
+#! usFunctionGenerateModuleInit(module_srcs
+#!                              NAME "My Module"
+#!                              LIBRARY_NAME "mylib"
+#!                              VERSION "1.2.0"
+#!                             )
+#! add_library(mylib ${module_srcs})
+#! \endverbatim
+#!
+#! \param src_var (required) The name of a list variable to which the path of the generated
+#!        source file will be appended.
+#! \param NAME (required) A human-readable name for the module.
+#! \param LIBRARY_NAME (optional) The name of the module, without extension. If empty, the
+#!        NAME argument will be used.
+#! \param DEPENDS (optional) A string containing module dependencies.
+#! \param VERSION (optional) A version string for the module.
+#!
 function(usFunctionGenerateModuleInit src_var)
 
 MACRO_PARSE_ARGUMENTS(US_MODULE "NAME;LIBRARY_NAME;DEPENDS;VERSION" "EXECUTABLE" ${ARGN})

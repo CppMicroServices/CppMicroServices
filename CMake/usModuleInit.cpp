6649f4f0cc8b81ea7946a97280d95f7da85eaef7
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2012-04-16 12:41:56 +0200

    New macro for initializing a module.

--- CMake/usModuleInit.cpp
+++ CMake/usModuleInit.cpp
@@ -19,66 +19,6 @@
 
 =============================================================================*/
 
-#include <usStaticInit_p.h>
+#include <usModuleInitialization.h>
 
-#include <usModuleRegistry.h>
-#include <usModuleContext.h>
-#include <usModule.h>
-#include <usModuleInfo.h>
-#include <usModuleUtils_p.h>
-
-US_BEGIN_NAMESPACE
-
-US_GLOBAL_STATIC_WITH_ARGS(ModuleInfo, moduleInfo, ("@US_MODULE_NAME@", "@US_MODULE_LIBRARY_NAME@", "@US_MODULE_DEPENDS_STR@", "@US_MODULE_VERSION@"))
-
-class US_ABI_LOCAL ModuleInitializer {
-
-public:
-
-  ModuleInitializer()
-  {
-    std::string location = ModuleUtils::GetLibraryPath(moduleInfo()->libName, reinterpret_cast<void*>(moduleInfo));
-    std::string activator_func = "_us_module_activator_instance_";
-    activator_func.append(moduleInfo()->name);
-
-    moduleInfo()->location = location;
-
-    if (moduleInfo()->libName.empty())
-    {
-      // make sure we retrieve symbols from the executable, if "libName" is empty
-      location.clear();
-    }
-    moduleInfo()->activatorHook = reinterpret_cast<ModuleInfo::ModuleActivatorHook>(ModuleUtils::GetSymbol(location, activator_func.c_str()));
-
-    Register();
-  }
-
-  static void Register()
-  {
-    ModuleRegistry::Register(moduleInfo());
-  }
-
-  ~ModuleInitializer()
-  {
-    ModuleRegistry::UnRegister(moduleInfo());
-  }
-
-};
-
-ModuleContext* GetModuleContext()
-{
-  // make sure the module is registered
-  if (moduleInfo()->id == 0)
-  {
-    ModuleInitializer::Register();
-  }
-
-  return ModuleRegistry::GetModule(moduleInfo()->id)->GetModuleContext();
-}
-
-US_END_NAMESPACE
-
-static US_PREPEND_NAMESPACE(ModuleInitializer) coreModule;
-
-
-
+US_INITIALIZE_MODULE("@US_MODULE_NAME@", "@US_MODULE_LIBRARY_NAME@", "@US_MODULE_DEPENDS_STR@", "@US_MODULE_VERSION@")

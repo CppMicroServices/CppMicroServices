96cf50b768255ce969858d0fca64fab7c8b02325
Sascha Zelzer <s.zelzer@dkfz-heidelberg.de>
2014-03-14 11:23:27 +0100

    Fixed copyright header.

--- core/src/service/usServiceListenerHook_p.h
+++ core/src/service/usServiceListenerHook_p.h
@@ -1,18 +1,23 @@
-/*===================================================================
+/*=============================================================================
 
-BlueBerry Platform
+  Library: CppMicroServices
 
-Copyright (c) German Cancer Research Center,
-Division of Medical and Biological Informatics.
-All rights reserved.
+  Copyright (c) German Cancer Research Center,
+    Division of Medical and Biological Informatics
 
-This software is distributed WITHOUT ANY WARRANTY; without
-even the implied warranty of MERCHANTABILITY or FITNESS FOR
-A PARTICULAR PURPOSE.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-See LICENSE.txt or http://www.mitk.org for details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-===================================================================*/
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+=============================================================================*/
 
 #ifndef USSERVICELISTENERHOOK_P_H
 #define USSERVICELISTENERHOOK_P_H

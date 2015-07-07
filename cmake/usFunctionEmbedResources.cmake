5aa602300cbe6927aac347f6704c3d9c111143c6
Sascha Zelzer <sascha.zelzer@gmail.com>
2015-07-08 00:01:42 +0200

    Fixed embedding init.scm file.

--- cmake/usFunctionEmbedResources.cmake
+++ cmake/usFunctionEmbedResources.cmake
@@ -7,7 +7,7 @@
 #!
 #! \note To set-up correct file dependencies from your module target to your resource
 #!       files, you have to add a special source file to the source list of the target.
-#!       The source file name can be retrieved by using usFunctionGetResourceSoruce.
+#!       The source file name can be retrieved by using usFunctionGetResourceSource.
 #!       This ensures that changed resource files will automatically be re-added to the
 #!       module.
 #!
@@ -23,9 +23,9 @@
 #! \code{.cmake}
 #! set(module_srcs )
 #! usFunctionEmbedResources(TARGET mylib
-#!                        MODULE_NAME org_me_mylib
-#!                        FILES config.properties logo.png
-#!                       )
+#!                          MODULE_NAME org_me_mylib
+#!                          FILES config.properties logo.png
+#!                         )
 #! \endcode
 #!
 #! \param TARGET (required) The target to which the resource files are added.

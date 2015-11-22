1d846b803ce9e986ec54b21a904870363605de79
Sascha Zelzer <sascha.zelzer@gmail.com>
2015-11-22 22:38:07 +0100

    Define out-of-line destructor for ServiceException.

--- core/include/usServiceException.h
+++ core/include/usServiceException.h
@@ -92,7 +92,7 @@ public:
   ServiceException(const ServiceException& o);
   ServiceException& operator=(const ServiceException& o);
 
-  ~ServiceException() throw() { }
+  ~ServiceException();
 
   /**
    * Returns the type for this exception or <code>UNSPECIFIED</code> if the

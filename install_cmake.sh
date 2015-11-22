09cb89e83a80f07c745fd7d25345a75cfc39a9c3
Sascha Zelzer <sascha.zelzer@gmail.com>
2015-11-22 10:52:05 +0100

    Travis: Use container and caching support for custom CMake install.
    Signed-off-by: Sascha Zelzer <sascha.zelzer@gmail.com>

---
+++ install_cmake.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+if [ ! -d "$HOME/cache" ]; then
+  mkdir $HOME/cache;
+fi
+
+# check to see if CMake is cached
+if [ ! -f "$HOME/cache/bin/ctest" ]; then
+
+  wget https://cmake.org/files/v2.8/cmake-2.8.12.2-Linux-i386.tar.gz -O /tmp/cmake.tar.gz
+  tar -xzvf /tmp/cmake.tar.gz -C /tmp
+  mv /tmp/cmake-2.8.12.2-Linux-i386/* $HOME/cache/
+else
+  echo 'Using cached CMake installation.';
+fi

39e70f0185c0adc09650885af7a4115bdf0ca1f5
Sascha Zelzer <sascha.zelzer@gmail.com>
2015-11-22 10:54:35 +0100

    Travis: Do not use https urls.
    Signed-off-by: Sascha Zelzer <sascha.zelzer@gmail.com>

--- install_cmake.sh
+++ install_cmake.sh
@@ -7,7 +7,7 @@ fi
 # check to see if CMake is cached
 if [ ! -f "$HOME/cache/bin/ctest" ]; then
 
-  wget https://cmake.org/files/v2.8/cmake-2.8.12.2-Linux-i386.tar.gz -O /tmp/cmake.tar.gz
+  wget http://cmake.org/files/v2.8/cmake-2.8.12.2-Linux-i386.tar.gz -O /tmp/cmake.tar.gz
   tar -xzvf /tmp/cmake.tar.gz -C /tmp
   mv /tmp/cmake-2.8.12.2-Linux-i386/* $HOME/cache/
 else

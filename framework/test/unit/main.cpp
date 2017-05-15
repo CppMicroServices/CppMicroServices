92412748e42b0416515d5387707605d2193bf729
CSol314 <cindysol@gmail.com>
2017-05-15 16:05:10 -0400

    GTest Integration

---
+++ framework/test/unit/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+
+int main(int argc, char * argv[])
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}

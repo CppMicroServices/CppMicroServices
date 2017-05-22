1d0c4d1072c7b4a8a6c1d81056bdf1ab59bf14c4
karthikreddy09 <karthikreddy09@icloud.com>
2017-05-22 17:25:36 -0400

    Skip Unicode test on MINGW
    Signed-off-by: The Mathworks Inc <Roy.Lurie@mathworks.com>

--- framework/test/BundleTest.cpp
+++ framework/test/BundleTest.cpp
@@ -612,8 +612,8 @@ void TestAutoInstallEmbeddedBundles()
 
 void TestUnicodePaths()
 {
-  // skip this test if building static libraries or if using compiler lower than Visual Studio 2015
-#if !defined(US_BUILD_SHARED_LIBS) || (defined(_MSC_VER) && (_MSC_VER < 1900))
+  // skip this test if building static libraries or using MINGW evironment or using Visual Studio < VS2015
+#if !defined(US_BUILD_SHARED_LIBS) || defined(__MINGW32__) || (defined(_MSC_VER) && (_MSC_VER < 1900))
   US_TEST_OUTPUT( << "Skipping test point for unicode path");
 #else
   FrameworkFactory factory;

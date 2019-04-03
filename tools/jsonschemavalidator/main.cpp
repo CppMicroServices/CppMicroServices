1d46f449b90f8d00003dabc9d3f6b2d339f26230
Abhinay Reddyreddy <karthikreddy09@icloud.com>
2019-04-03 10:57:20 -0400

    Incorporated feedback and ran clang-format
    Signed-off-by: The Mathworks Inc <Roy.Lurie@mathworks.com>

--- tools/jsonschemavalidator/main.cpp
+++ tools/jsonschemavalidator/main.cpp
@@ -20,28 +20,18 @@ limitations under the License.
 
 =============================================================================*/
 
+#include <fstream>   // std::ifstream
+#include <iostream>  // std::cerr
+#include <memory>    // std::unique_ptr
+#include <stdexcept> // std::runtime_error
+#include <string>    // std::to_string
+
 #include "optionparser.h"
 #include "rapidjson/error/en.h"
-#include "rapidjson/filereadstream.h"
+#include "rapidjson/istreamwrapper.h" // depends on ifstream included above
+#include "rapidjson/prettywriter.h"
 #include "rapidjson/schema.h"
 #include "rapidjson/stringbuffer.h"
-#include "rapidjson/prettywriter.h"
-#include <memory>
-#include <iostream>
-#include <string>
-
-using namespace rapidjson;
-
-// functor object for deleting FILE objects
-struct FILEDeleter
-{
-  typedef FILE* pointer;
-  void operator()(FILE *pFile)
-  {
-    if (pFile)
-      fclose(pFile);
-  }
-};
 
 /**
  * \brief Utility function to create a rapidjson::Document object from a file path
@@ -51,21 +41,20 @@ struct FILEDeleter
  * \returns rapidjson::Document object representing the json file
  * \throws std::runtime_error if the input file is non-existent or is not a valid json file
  */
-Document ReadJSONDocument(const std::string& filePath)
+rapidjson::Document ReadJSONDocument(const std::string& filePath)
 {
-  std::unique_ptr<FILE, FILEDeleter> fp(fopen(filePath.c_str(), "r"));
-  if (!fp)
-  {
-    std::string err("File '" + filePath + "' not found");
-    throw std::runtime_error(err);
+  std::ifstream fileStream(filePath.c_str());
+  if (!fileStream.is_open()) {
+    throw std::runtime_error("Failed to open file at " + filePath);
   }
-  char buffer[4096];
-  FileReadStream fs(fp.get(), buffer, sizeof(buffer));
-  Document doc;
-  doc.ParseStream(fs);
-  if(doc.HasParseError())
-  {
-    throw std::runtime_error("File '" + filePath + "' is not a valid JSON\n Error(offset " + std::to_string(doc.GetErrorOffset()) + "): " + GetParseError_En(doc.GetParseError()));
+  rapidjson::IStreamWrapper streamWrapper(fileStream);
+  rapidjson::Document doc;
+  doc.ParseStream(streamWrapper);
+  if (doc.HasParseError()) {
+    throw std::runtime_error("File '" + filePath +
+                             "' is not a valid JSON\n Error(offset " +
+                             std::to_string(doc.GetErrorOffset()) +
+                             "): " + GetParseError_En(doc.GetParseError()));
   }
   return doc;
 }
@@ -80,29 +69,28 @@ Document ReadJSONDocument(const std::string& filePath)
  *          if the json file matches the schema, error otherwise. In case of
  *          error, the second element contains the error report
  */
-std::pair<bool, std::string> validate(const std::string& jsonfile, const std::string& schemafile) noexcept
+std::pair<bool, std::string> validate(const std::string& jsonfile,
+                                      const std::string& schemafile) noexcept
 {
-  try
-  {
-    SchemaDocument sd(ReadJSONDocument(schemafile));
-    SchemaValidator validator(sd);
-    Document json = ReadJSONDocument(jsonfile);
-    if (!json.Accept(validator))
-    {
-      std::string errorMsg("JSON file " + jsonfile + " does not match schema file " + schemafile);
-      StringBuffer sb;
-      PrettyWriter<StringBuffer> w(sb);
+  try {
+    rapidjson::SchemaDocument sd(ReadJSONDocument(schemafile));
+    rapidjson::SchemaValidator validator(sd);
+    rapidjson::Document json = ReadJSONDocument(jsonfile);
+    if (!json.Accept(validator)) {
+      std::string errorMsg("JSON file " + jsonfile +
+                           " does not match schema file " + schemafile);
+      rapidjson::StringBuffer sb;
+      rapidjson::PrettyWriter<rapidjson::StringBuffer> w(sb);
       validator.GetError().Accept(w);
       errorMsg.append("\nError report:\n");
       errorMsg.append(sb.GetString());
       throw std::runtime_error(errorMsg);
     }
-  }
-  catch(std::exception& e)
-  {
+  } catch (const std::exception& e) {
     return std::make_pair(false, e.what());
   }
-  return std::make_pair(true, "JSON file" + jsonfile + " matches the schema " + schemafile);
+  return std::make_pair(
+    true, "JSON file" + jsonfile + " matches the schema " + schemafile);
 }
 
 /**
@@ -116,12 +104,11 @@ struct Custom_Arg : public option::Arg
   static option::ArgStatus NonEmpty(const option::Option& option, bool msg)
   {
     auto retVal = option::ARG_OK;
-    if (option.arg == 0 || option.arg[0] == 0)
-    {
+    if (option.arg == 0 || option.arg[0] == 0) {
       retVal = option::ARG_ILLEGAL;
-      if (msg)
-      {
-        std::cerr << "ERROR: Option '" << option.name << "' requires a non-empty argument" << std::endl;
+      if (msg) {
+        std::cerr << "ERROR: Option '" << option.name
+                  << "' requires a non-empty argument" << std::endl;
       }
     }
     return retVal;
@@ -165,37 +152,35 @@ const option::Descriptor usage[] = {
   { 0, 0, 0, 0, 0, 0 }
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   argc -= (argc > 0);
   argv += (argc > 0); // skip program name in argv[0]
   option::Stats stats(usage, argc, argv);
-  std::unique_ptr<option::Option[]> options(new option::Option[stats.options_max]);
-  std::unique_ptr<option::Option[]> buffer(new option::Option[stats.buffer_max]);
+  std::unique_ptr<option::Option[]> options(
+    new option::Option[stats.options_max]);
+  std::unique_ptr<option::Option[]> buffer(
+    new option::Option[stats.buffer_max]);
   option::Parser parse(true, usage, argc, argv, options.get(), buffer.get());
 
   auto retVal = EXIT_SUCCESS;
-  if (argc == 0 || options[HELP])
-  {
+  if (argc == 0 || options[HELP]) {
     option::printUsage(std::clog, usage);
-  }
-  else if (!options[SCHEMAFILE])
-  {
-    std::cerr << "A schema file must be specified using the --schema-file(-s) option. Check usage." << std::endl;
+  } else if (!options[SCHEMAFILE]) {
+    std::cerr << "A schema file must be specified using the --schema-file(-s) "
+                 "option. Check usage."
+              << std::endl;
     retVal = EXIT_FAILURE;
-  }
-  else if (!options[JSONFILE])
-  {
-    std::cerr << "A json file must be specified using the --json-file(-j) option. Check usage." << std::endl;
+  } else if (!options[JSONFILE]) {
+    std::cerr << "A json file must be specified using the --json-file(-j) "
+                 "option. Check usage."
+              << std::endl;
     retVal = EXIT_FAILURE;
-  }
-  else
-  {
+  } else {
     std::string jsonfilepath(options[JSONFILE].arg);
     std::string schemafilepath(options[SCHEMAFILE].arg);
     auto result = validate(jsonfilepath, schemafilepath);
-    if(!result.first)
-    {
+    if (!result.first) {
       std::cerr << result.second << std::endl;
       retVal = EXIT_FAILURE;
     }

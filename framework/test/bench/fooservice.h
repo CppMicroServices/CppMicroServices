697083736cb604a754bcd4e7257b1f61eab6cffb
Jeff DiClemente <jeff.diclemente@mathworks.com>
2019-06-03 14:36:04 -0400

    Remove use of CPPMICROSERVICES_DECLARE_SERIVCE_INTERFACE macro

--- framework/test/bench/fooservice.h
+++ framework/test/bench/fooservice.h
@@ -1,18 +1,14 @@
 
-#include <cppmicroservices/ServiceInterface.h>
-
 // A dummy interface and impl to use with service trackers
-namespace {
-    class Foo {
-      public:
-        virtual ~Foo() = default;
-    };
-    
-    class FooImpl : public Foo {
-    };
-}
+namespace googlebenchmark {
+namespace test {
+class Foo
+{
+public:
+  virtual ~Foo() = default;
+};
 
-// Since the Foo interface is embedded in the test executable, its symbols are
-// not exported. Using CPPMICROSERVICES_DECLARE_SERVICE_INTERFACE ensures that
-// the symbols are exported correctly for use by CppMicroServices.
-CPPMICROSERVICES_DECLARE_SERVICE_INTERFACE(Foo, "org.cppmicroservices.googlebenchmark.test.Foo");
+class FooImpl : public Foo
+{};
+}
+}

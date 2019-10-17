579054f3a3593f5a67d9ddf45065185873804a96
Rick Yu <jeeyuen@hotmail.com>
2019-10-17 16:55:53 -0400

    Update CMakLists.txt and fix complier errors
    Update CMakeLists.txt to include the following new tests:
    BundleEventTest.cpp
    BundleResourceTest.cpp
    BundleStreamOperatorTest.cpp
    ServiceEventStreamOperatorTest.cpp
    
    Fix Setup and Teardown complier errors for BundleEventTest.cpp and BundleResourceTest.cpp on Mac
    
    Fix unsigned and signed int comparison complier errors for BundleResourceTest.cpp on Linux and Mac

--- framework/test/gtest/BundleEventTest.cpp
+++ framework/test/gtest/BundleEventTest.cpp
@@ -42,7 +42,7 @@ public:
 
   ~BundleEventTest() override = default;
 
-  virtual void SetUp()
+  void SetUp() override
   {
     f.Start();
 
@@ -55,7 +55,7 @@ public:
 #endif
   }
 
-  virtual void TearDown()
+  void TearDown() override
   {
     f.Stop();
     f.WaitForStop(std::chrono::milliseconds::zero());

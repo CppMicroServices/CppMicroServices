5f4a247ca3d2ef6f846b71805a8148325bf20083
Michael Carney <mike@carneyweb.com>
2019-11-06 10:21:54 -0500

    Updated GetService() to deal with empty interface maps

--- framework/src/service/ServiceObjects.cpp
+++ framework/src/service/ServiceObjects.cpp
@@ -138,12 +138,21 @@ std::shared_ptr<void> ServiceObjectsBase::GetService() const
     return nullptr;
   }
 
-  std::shared_ptr<UngetHelper> h(
-    new UngetHelper(d->GetServiceInterfaceMap(),
-                    d->m_reference,
-                    d->m_context->bundle->shared_from_this()));
-  return std::shared_ptr<void>(
-    h, (h->interfaceMap->find(d->m_reference.GetInterfaceId()))->second.get());
+  // interfaceMap can be null under certain circumstance
+  // e.g. if a constructor is thrown in the service implementation.
+  // In that case, we bail out early.
+  auto interfaceMap = d->GetServiceInterfaceMap();
+  if (!interfaceMap) {
+    return nullptr;
+  }
+  
+  auto h = std::make_shared<UngetHelper>(interfaceMap
+                                         , d->m_reference
+                                         , d->m_context->bundle->shared_from_this());
+  auto deleter = h->interfaceMap
+                  ->find(d->m_reference.GetInterfaceId())
+                  ->second.get()
+  return std::shared_ptr<void>(h, deleter);
 }
 
 InterfaceMapConstPtr ServiceObjectsBase::GetServiceInterfaceMap() const

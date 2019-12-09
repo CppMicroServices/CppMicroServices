e5c4466e1d9e806c52ef7667d9590bdeb5736a07
achristoforides <38366659+achristoforides@users.noreply.github.com>
2019-12-09 12:18:30 -0500

    Removed GetChildren() dependency of GetChildResources()
    Signed-off-by: The Mathworks Inc <alchrist@mathworks.com>

--- framework/test/gtest/BundleResourceTest.cpp
+++ framework/test/gtest/BundleResourceTest.cpp
@@ -71,10 +71,10 @@ TEST(BundleResourceTestNoBundleInstall, getChildResourcesFromInvalidBundle)
 TEST_F(BundleResourceTest, getChildResources)
 {
   BundleResource resource = bundleR.GetResource("icons/");
-  ASSERT_EQ(resource.GetChildResources().size(), static_cast<unsigned int>(0));
+  // Demonstrate that call to GetChildren() is not required to get the actual
+  // count of children resources
+  ASSERT_NE(resource.GetChildResources().size(), static_cast<unsigned int>(0));
 
-  // GetChildResources() should not have a dependency on GetChildren()
-  // to return the correct size
-  resource.GetChildren();
+  // Confirm that GetChildResources() returns the correct number
   ASSERT_EQ(resource.GetChildResources().size(), static_cast<unsigned int>(3));
 }

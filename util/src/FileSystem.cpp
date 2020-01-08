2e7c4cb179ddf7e21b438715b14e716f3de15363
Michael Carney <mike@carneyweb.com>
2020-01-08 14:46:03 -0500

    more reformatting.

--- util/src/FileSystem.cpp
+++ util/src/FileSystem.cpp
@@ -79,12 +79,12 @@ bool not_found_win32_error(int errval)
 {
   return (errval == ERROR_FILE_NOT_FOUND
           || errval == ERROR_PATH_NOT_FOUND
-          || errval == ERROR_INVALID_NAME // "//foo"
-          || errval == ERROR_INVALID_DRIVE // USB card reader with no card inserted
-          || errval == ERROR_NOT_READY  // CD/DVD drive with no disc inserted
+          || errval == ERROR_INVALID_NAME      // "//foo"
+          || errval == ERROR_INVALID_DRIVE     // USB card reader with no card inserted
+          || errval == ERROR_NOT_READY         // CD/DVD drive with no disc inserted
           || errval == ERROR_INVALID_PARAMETER // ":sys:stat.h"
           || errval == ERROR_BAD_PATHNAME      // "//nosuch" on Win64
-          || errval == ERROR_BAD_NETPATH);      // "//nosuch" on Win32
+          || errval == ERROR_BAD_NETPATH);     // "//nosuch" on Win32
 }
 #endif
 

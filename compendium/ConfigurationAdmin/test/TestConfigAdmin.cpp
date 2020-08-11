51a31474a819479f3d63dfbcfb9a1fa1953cc7dd
achristoforides <38366659+achristoforides@users.noreply.github.com>
2020-08-11 12:39:50 -0400

    Updated timeout again
    Signed-off-by: The MathWorks, Inc. <alchrist@mathworks.com>

--- compendium/ConfigurationAdmin/test/TestConfigAdmin.cpp
+++ compendium/ConfigurationAdmin/test/TestConfigAdmin.cpp
@@ -22,7 +22,7 @@ namespace cm = cppmicroservices::service::cm;
 
 namespace {
 
-auto const DEFAULT_POLL_PERIOD = std::chrono::milliseconds(60000);
+auto const DEFAULT_POLL_PERIOD = std::chrono::milliseconds(90000);
 
 std::string PathToLib(const std::string& libName)
 {

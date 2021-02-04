96432f031ebd9d7f0e56f0b8ce26c58cfa64acf6
Rick <jeeyuen@hotmail.com>
2021-02-04 15:44:21 -0500

    Revert "remove hidden chars"
    This reverts commit 8ab57f2940a788a191f937cbbadc3c734520e2b8.

--- compendium/test_bundles/TestBundleDSDGOU/src/ServiceImpl.hpp
+++ compendium/test_bundles/TestBundleDSDGOU/src/ServiceImpl.hpp
@@ -16,7 +16,7 @@ public:
   ServiceComponentDynamicGreedyOptionalUnary() = default;
   ~ServiceComponentDynamicGreedyOptionalUnary() = default;
   virtual std::string ExtendedDescription() override;
-
+  
   void Activate(const std::shared_ptr<ComponentContext>&);
   void Deactivate(const std::shared_ptr<ComponentContext>&);
 

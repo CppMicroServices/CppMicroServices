4b11f88c4a129b544591a182b6a3446a8874fcd6
Rick <jeeyuen@hotmail.com>
2021-02-04 12:03:36 -0500

    Get rid of line 32

--- compendium/test_bundles/TestBundleDSDGMU/src/ServiceComponents.hpp
+++ compendium/test_bundles/TestBundleDSDGMU/src/ServiceComponents.hpp
@@ -2,5 +2,25 @@
 #define SERVICECOMPONENTS_HPP
 
 #include "ServiceImpl.hpp"
+namespace Dummy
+{
+    class MyClass
+    {
+    public:
+        MyClass();
+        ~MyClass();
+
+    private:
+
+    };
+
+    MyClass::MyClass()
+    {
+    }
+
+    MyClass::~MyClass()
+    {
+    }
+}
 
 #endif
--- compendium/test_bundles/TestBundleDSDGMU/src/ServiceImpl.hpp
+++ compendium/test_bundles/TestBundleDSDGMU/src/ServiceImpl.hpp
@@ -1,32 +1,23 @@
 #ifndef _SERVICE_IMPL_HPP_
 #define _SERVICE_IMPL_HPP_
-
 #include <mutex>
-
 #include "cppmicroservices/servicecomponent/ComponentContext.hpp"
 #include "TestInterfaces/Interfaces.hpp"
-
 using ComponentContext = cppmicroservices::service::component::ComponentContext;
-
 namespace sample {
-
 class ServiceComponentDynamicGreedyMandatoryUnary final : public test::Interface2
 {
 public:
   ServiceComponentDynamicGreedyMandatoryUnary() = default;
   ~ServiceComponentDynamicGreedyMandatoryUnary() = default;
   virtual std::string ExtendedDescription() override;
-  
   void Activate(const std::shared_ptr<ComponentContext>&);
   void Deactivate(const std::shared_ptr<ComponentContext>&);
-
   void Bindfoo(const std::shared_ptr<test::Interface1>&);
   void Unbindfoo(const std::shared_ptr<test::Interface1>&);
 private:
   std::shared_ptr<test::Interface1> foo;
   std::mutex fooMutex;
 };
-
 } // namespaces
-
 #endif // _SERVICE_IMPL_HPP_

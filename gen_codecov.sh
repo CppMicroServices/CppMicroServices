466549a6b83a7c5c5cadc20a3f0bd4a6effbea2f
Rick <jeeyuen@hotmail.com>
2021-02-09 11:16:17 -0500

    Revert "Update gen_codecov.sh"
    This reverts commit fb753182d6aa8bfc8476c97ff25ccf0eff56f48a.

--- gen_codecov.sh
+++ gen_codecov.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 GCOV_EXE=${MY_COVERAGE} # gcov and g++ versions must match
 
-for filename in `find . -name "*.cpp"`;
+for filename in `find . | egrep '\.cpp'`;
 do
-  $GCOV_EXE -o . $filename;
+  $GCOV_EXE -n -o . $filename > /dev/null;
 done
+

02c305d1acc074ba357e8cbde370f5823c98d9d8
Aaditya Patil <apatil19@binghamton.edu>
2021-03-18 18:13:55 -0400

    Changes from code review
    Changes:
    - Clang formatted source files
    - Removed unnecessary conditions
    
    Signed-off-by: The MathWorks, Inc. aadityap@mathworks.com

--- framework/test/gtest/SharedLibraryTest.cpp
+++ framework/test/gtest/SharedLibraryTest.cpp
@@ -20,93 +20,88 @@
 
 =============================================================================*/
 
-#include "cppmicroservices/util/FileSystem.h"
 #include "cppmicroservices/SharedLibrary.h"
 #include "TestUtils.h"
 #include "TestingConfig.h"
+#include "cppmicroservices/util/FileSystem.h"
 #include "gtest/gtest.h"
 
 using namespace cppmicroservices;
 
-TEST(SharedLibraryTest,TestSharedLibraryOperations)
+TEST(SharedLibraryTest, TestSharedLibraryOperations)
 {
-    auto lib1Name = "TestBundleA" US_LIB_POSTFIX;
-    const std::string libAFilePath = cppmicroservices::testing::LIB_PATH
-        + util::DIR_SEP
-        + US_LIB_PREFIX
-        + lib1Name
-        + US_LIB_EXT;
-    SharedLibrary lib1(libAFilePath);
+  auto lib1Name = "TestBundleA" US_LIB_POSTFIX;
+  const std::string libAFilePath = cppmicroservices::testing::LIB_PATH +
+                                   util::DIR_SEP + US_LIB_PREFIX + lib1Name +
+                                   US_LIB_EXT;
+  SharedLibrary lib1(libAFilePath);
 
-    ASSERT_EQ(lib1.GetFilePath(), libAFilePath);
-    ASSERT_EQ(lib1.GetLibraryPath(), cppmicroservices::testing::LIB_PATH);
-    ASSERT_EQ(lib1.GetName(), lib1Name);
-    ASSERT_EQ(lib1.GetPrefix(), US_LIB_PREFIX);
-    ASSERT_EQ(lib1.GetSuffix(), US_LIB_EXT);
-    
-    lib1.SetName("bla");
-    ASSERT_EQ(lib1.GetName(), lib1Name);
-    lib1.SetLibraryPath("bla");
-    ASSERT_EQ(lib1.GetLibraryPath(), cppmicroservices::testing::LIB_PATH);
-    lib1.SetPrefix("bla");
-    ASSERT_EQ(lib1.GetPrefix(), US_LIB_PREFIX);
-    lib1.SetSuffix("bla");
-    ASSERT_EQ(lib1.GetSuffix(), US_LIB_EXT);
-    ASSERT_EQ(lib1.GetFilePath(), libAFilePath);
+  ASSERT_EQ(lib1.GetFilePath(), libAFilePath);
+  ASSERT_EQ(lib1.GetLibraryPath(), cppmicroservices::testing::LIB_PATH);
+  ASSERT_EQ(lib1.GetName(), lib1Name);
+  ASSERT_EQ(lib1.GetPrefix(), US_LIB_PREFIX);
+  ASSERT_EQ(lib1.GetSuffix(), US_LIB_EXT);
 
-    lib1.SetFilePath("bla");
-    ASSERT_EQ(lib1.GetFilePath(), "bla"); 
-    ASSERT_TRUE(lib1.GetLibraryPath().empty());
-    ASSERT_EQ(lib1.GetName(), "bla");
-    ASSERT_EQ(lib1.GetPrefix(), US_LIB_PREFIX);
-    ASSERT_EQ(lib1.GetSuffix(), US_LIB_EXT);
+  lib1.SetName("bla");
+  ASSERT_EQ(lib1.GetName(), lib1Name);
+  lib1.SetLibraryPath("bla");
+  ASSERT_EQ(lib1.GetLibraryPath(), cppmicroservices::testing::LIB_PATH);
+  lib1.SetPrefix("bla");
+  ASSERT_EQ(lib1.GetPrefix(), US_LIB_PREFIX);
+  lib1.SetSuffix("bla");
+  ASSERT_EQ(lib1.GetSuffix(), US_LIB_EXT);
+  ASSERT_EQ(lib1.GetFilePath(), libAFilePath);
 
-    ASSERT_THROW(lib1.Load(), std::runtime_error);
-    ASSERT_EQ(lib1.IsLoaded(), false);
-    ASSERT_EQ(lib1.GetHandle(), nullptr);
+  lib1.SetFilePath("bla");
+  ASSERT_EQ(lib1.GetFilePath(), "bla");
+  ASSERT_TRUE(lib1.GetLibraryPath().empty());
+  ASSERT_EQ(lib1.GetName(), "bla");
+  ASSERT_EQ(lib1.GetPrefix(), US_LIB_PREFIX);
+  ASSERT_EQ(lib1.GetSuffix(), US_LIB_EXT);
 
-    lib1.SetFilePath(libAFilePath);
-    lib1.Load();
-    ASSERT_TRUE(lib1.IsLoaded());
-    ASSERT_NE(lib1.GetHandle(), nullptr);
-    ASSERT_THROW(lib1.Load(), std::logic_error);
+  ASSERT_THROW(lib1.Load(), std::runtime_error);
+  ASSERT_EQ(lib1.IsLoaded(), false);
+  ASSERT_EQ(lib1.GetHandle(), nullptr);
 
-    lib1.SetFilePath("bla");
-    ASSERT_EQ(lib1.GetFilePath(), libAFilePath);
-    lib1.Unload();
+  lib1.SetFilePath(libAFilePath);
+  lib1.Load();
+  ASSERT_TRUE(lib1.IsLoaded());
+  ASSERT_NE(lib1.GetHandle(), nullptr);
+  ASSERT_THROW(lib1.Load(), std::logic_error);
 
-    auto lib2Name = "TestBundleA"  US_LIB_POSTFIX;
-    SharedLibrary lib2(cppmicroservices::testing::LIB_PATH, lib2Name);
-    ASSERT_EQ(lib2.GetFilePath(), libAFilePath);
-    lib2.SetPrefix("");
-    ASSERT_TRUE(lib2.GetPrefix().empty());
-    ASSERT_EQ(lib2.GetFilePath(), (cppmicroservices::testing::LIB_PATH
-        + util::DIR_SEP
-        + "TestBundleA"
-        + US_LIB_POSTFIX
-        + US_LIB_EXT));
+  lib1.SetFilePath("bla");
+  ASSERT_EQ(lib1.GetFilePath(), libAFilePath);
+  lib1.Unload();
 
-    SharedLibrary lib3 = lib2;
-    ASSERT_EQ(lib3.GetFilePath(), lib2.GetFilePath());
-    lib3.SetPrefix(US_LIB_PREFIX);
-    ASSERT_EQ(lib3.GetFilePath(), libAFilePath);
-    lib3.Load();
-    ASSERT_TRUE(lib3.IsLoaded());
-    ASSERT_TRUE(lib2.IsLoaded());
-    lib1 = lib3;
-    ASSERT_THROW(lib1.Load(), std::logic_error);
+  auto lib2Name = "TestBundleA" US_LIB_POSTFIX;
+  SharedLibrary lib2(cppmicroservices::testing::LIB_PATH, lib2Name);
+  ASSERT_EQ(lib2.GetFilePath(), libAFilePath);
+  lib2.SetPrefix("");
+  ASSERT_TRUE(lib2.GetPrefix().empty());
+  ASSERT_EQ(lib2.GetFilePath(),
+            (cppmicroservices::testing::LIB_PATH + util::DIR_SEP +
+             "TestBundleA" + US_LIB_POSTFIX + US_LIB_EXT));
 
-    lib3.Unload();
-    ASSERT_FALSE(lib3.IsLoaded());
-    ASSERT_FALSE(lib1.IsLoaded());
+  SharedLibrary lib3 = lib2;
+  ASSERT_EQ(lib3.GetFilePath(), lib2.GetFilePath());
+  lib3.SetPrefix(US_LIB_PREFIX);
+  ASSERT_EQ(lib3.GetFilePath(), libAFilePath);
+  lib3.Load();
+  ASSERT_TRUE(lib3.IsLoaded());
+  ASSERT_TRUE(lib2.IsLoaded());
+  lib1 = lib3;
+  ASSERT_THROW(lib1.Load(), std::logic_error);
 
-    // gcov on Mac OS X writes coverage files during static destruction
-    // resulting in a crash if a dylib is completely unloaded from the process.
-    // https://bugs.llvm.org/show_bug.cgi?id=27224
-    #if !defined(US_PLATFORM_APPLE) || !defined(US_COVERAGE_ENABLED)
-        lib2.Unload();
-        ASSERT_FALSE(lib2.IsLoaded());
-    #endif
-        lib1.Unload();
+  lib3.Unload();
+  ASSERT_FALSE(lib3.IsLoaded());
+  ASSERT_FALSE(lib1.IsLoaded());
+
+// gcov on Mac OS X writes coverage files during static destruction
+// resulting in a crash if a dylib is completely unloaded from the process.
+// https://bugs.llvm.org/show_bug.cgi?id=27224
+#if !defined(US_PLATFORM_APPLE) || !defined(US_COVERAGE_ENABLED)
+  lib2.Unload();
+  ASSERT_FALSE(lib2.IsLoaded());
+#endif
+  lib1.Unload();
 }
-

5dd351392cff1454b84b1ed77da22234e4efbadb
Rick <jeeyuen@hotmail.com>
2021-04-27 14:10:22 -0400

    Add custom component name test
    TestBundleDSCA16
    - configuration-policy = require
    - immediate = true
    - specify component name
    
    TestComponentNameWithPID.cpp
    - add test using custom component name
    
    Signed-off-by: The MathWorks, Inc. rickyu@mathworks.com

---
+++ compendium/test_bundles/TestBundleDSCA16/src/ServiceImpl.cpp
@@ -0,0 +1,12 @@
+#include "ServiceImpl.hpp"
+#include <iostream>
+
+namespace sample {
+
+cppmicroservices::AnyMap ServiceComponentCA16::GetProperties()
+{
+  std::lock_guard<std::mutex> lock(propertiesLock);
+  return properties;
+}
+
+}

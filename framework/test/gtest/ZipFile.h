a80da7cdc50fd23a4062f5065a413089acbd045c
Aaditya Patil <apatil19@binghamton.edu>
2021-06-03 15:33:07 -0400

    Migrate ResourceCompilerTest to gtest
    Signed-off-by: The MathWorks, Inc. aadityap@mathworks.com

--- framework/test/gtest/ZipFile.h
+++ framework/test/gtest/ZipFile.h
@@ -24,6 +24,8 @@ limitations under the License.
 
 #include <cstring>
 #include <vector>
+#include <string>
+#include "gtest/gtest.h"
 
 /*
 * @brief Struct which represents an entry in the zip archive.

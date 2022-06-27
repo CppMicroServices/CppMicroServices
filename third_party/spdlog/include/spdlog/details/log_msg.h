a4bb5260c5ac9bac1fe7929d09aec4141a9336ff
Alexander Christoforides <38366659+achristoforides@users.noreply.github.com>
2022-06-27 14:20:32 -0400

    Fixed compilation issue for arm macOS native compilation (#696)
    Signed-off-by: The MathWorks, Inc. <alchrist@mathworks.com>

--- third_party/spdlog/include/spdlog/details/log_msg.h
+++ third_party/spdlog/include/spdlog/details/log_msg.h
@@ -14,7 +14,6 @@ struct SPDLOG_API log_msg
     log_msg(log_clock::time_point log_time, source_loc loc, string_view_t logger_name, level::level_enum lvl, string_view_t msg);
     log_msg(source_loc loc, string_view_t logger_name, level::level_enum lvl, string_view_t msg);
     log_msg(string_view_t logger_name, level::level_enum lvl, string_view_t msg);
-    log_msg(const log_msg &other) = default;
 
     string_view_t logger_name;
     level::level_enum level{level::off};

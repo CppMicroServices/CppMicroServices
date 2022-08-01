c3e8e57d0416f3d8824d96148d771e0fc027caf1
xru192 <ryanxu123@gmail.com>
2022-08-01 16:51:27 -0400

    Implement concurrency tests

--- framework/test/gtest/BundleTrackerConcurrencyTest.cpp
+++ framework/test/gtest/BundleTrackerConcurrencyTest.cpp
@@ -25,7 +25,10 @@
 #include "cppmicroservices/FrameworkEvent.h"
 #include "cppmicroservices/FrameworkFactory.h"
 
+#include <future>
+
 #include "TestUtils.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #ifdef GetObject
@@ -66,12 +69,164 @@ public:
   }
 };
 
+MATCHER_P(HasName, name, "")
+{
+  return arg.GetSymbolicName() == name;
+}
+
+#ifdef US_ENABLE_THREADING_SUPPORT
+class MockCustomizer2 : public BundleTrackerCustomizer<>
+{
+public:
+  MOCK_METHOD(std::optional<Bundle>,
+              AddingBundle,
+              (const Bundle&, const BundleEvent&),
+              (override));
+  MOCK_METHOD(void,
+              ModifiedBundle,
+              (const Bundle&, const BundleEvent&, Bundle),
+              (override));
+  MOCK_METHOD(void,
+              RemovedBundle,
+              (const Bundle&, const BundleEvent&, Bundle),
+              (override));
+};
+
 TEST_F(BundleTrackerConcurrencyTest, ConcurrentOpenCloseWorks)
 {
-  //TODO
+  auto bundleTracker = std::make_unique<BundleTracker<>>(context, all_states);
+
+  size_t numThreads = std::thread::hardware_concurrency();
+  ASSERT_GT(numThreads, 0ull) << "number of threads is 0";
+  std::vector<std::future<void>> futures;
+  std::promise<void> gate;
+  auto gateFuture = gate.get_future().share();
+  for (size_t i = 0; i <= numThreads; ++i) {
+    futures.push_back(
+      std::async(std::launch::async, [i, &bundleTracker, &gateFuture]() {
+        gateFuture.get();
+        for (int n = 0; n < 1000; ++n) {
+          if (i % 2 == 0) {
+            bundleTracker->Open();
+          } else {
+            bundleTracker->Close();
+          }
+        }
+      }));
+  }
+
+  gate.set_value();
+
+  for (auto& asyncFuture : futures) {
+    asyncFuture.get();
+  }
+}
+
+TEST_F(BundleTrackerConcurrencyTest, OpeningTrackerWhileBundlesChangeWorks)
+{
+  auto customizer = std::make_shared<MockCustomizer2>();
+  auto stateMask = _CreateStateMask(Bundle::State::STATE_ACTIVE);
+  auto bundleTracker =
+    std::make_unique<BundleTracker<>>(context, stateMask, customizer);
+
+  size_t numThreads = std::thread::hardware_concurrency();
+  ASSERT_GT(numThreads, 0ull) << "number of threads is 0";
+  std::vector<std::future<void>> futures;
+  std::promise<void> gate;
+  auto gateFuture = gate.get_future().share();
+
+  auto future1 =
+    std::async(std::launch::async, [&bundleTracker, &gateFuture]() {
+      gateFuture.get();
+      bundleTracker->Open();
+    });
+
+  auto future2 = std::async(std::launch::async, [this, &gateFuture]() {
+    gateFuture.get();
+    cppmicroservices::testing::InstallLib(framework.GetBundleContext(),
+                                          "TestBundleA")
+      .Start();
+    cppmicroservices::testing::InstallLib(framework.GetBundleContext(),
+                                          "TestBundleA2")
+      .Start();
+    cppmicroservices::testing::InstallLib(framework.GetBundleContext(),
+                                          "TestBundleB")
+      .Start();
+    cppmicroservices::testing::InstallLib(framework.GetBundleContext(),
+                                          "TestBundleH")
+      .Start();
+    cppmicroservices::testing::InstallLib(framework.GetBundleContext(),
+                                          "TestBundleM")
+      .Start();
+    cppmicroservices::testing::InstallLib(framework.GetBundleContext(),
+                                          "TestBundleR")
+      .Start();
+  });
+
+  EXPECT_CALL(*customizer, AddingBundle).Times(7);
+  gate.set_value();
+
+  future1.get();
+  future2.get();
+
+  bundleTracker->Close();
 }
 
 TEST_F(BundleTrackerConcurrencyTest, NoRaceConditionForRemovingChangingBundle)
 {
-  //TODO
+  auto customizer = std::make_shared<MockCustomizer2>();
+  auto bundleTracker =
+    std::make_unique<BundleTracker<>>(context, all_states, customizer);
+  Bundle bundleA = cppmicroservices::testing::InstallLib(
+    framework.GetBundleContext(), "TestBundleA");
+  // make AddingBundle return nullopt for all bundles besides bundleA
+  EXPECT_CALL(*customizer, AddingBundle)
+    .WillRepeatedly(::testing::Return(std::nullopt));
+  EXPECT_CALL(*customizer, AddingBundle(HasName("TestBundleA"), ::testing::_))
+    .WillOnce(::testing::ReturnArg<0>());
+  bundleTracker->Open();
+
+  size_t numThreads = std::thread::hardware_concurrency();
+  ASSERT_GT(numThreads, 0ull) << "number of threads is 0";
+  std::promise<void> gate;
+  auto gateFuture = gate.get_future().share();
+  auto future1 =
+    std::async(std::launch::async, [&gateFuture, &bundleTracker, &bundleA]() {
+      gateFuture.get();
+      bundleTracker->Remove(bundleA);
+    });
+  auto future2 = std::async(std::launch::async, [&gateFuture, &bundleA]() {
+    gateFuture.get();
+    bundleA.Start();
+  });
+
+  // The Remove can occur while bundleA is in any state while being started,
+  // so all 4 of the following series of callbacks are possible:
+  //    Remove -> Add -> Modify -> Modify
+  //    Modify -> Remove -> Add -> Modify
+  //    Modify -> Modify -> Remove -> Add
+  //    Modify -> Modify -> Modify -> Remove
+
+  int counter = 0; // counts the number of AddingBundle and ModifiedBundle callbacks
+  auto increment = [&counter]() { counter++; };
+
+  EXPECT_CALL(*customizer, AddingBundle)
+    .Times(::testing::AtMost(1))
+    .WillOnce(::testing::DoAll(::testing::Invoke(increment),
+                               ::testing::ReturnArg<0>()));
+  EXPECT_CALL(*customizer, ModifiedBundle)
+    .Times(::testing::AtLeast(2))
+    .WillRepeatedly(::testing::Invoke(increment));
+  EXPECT_CALL(*customizer, RemovedBundle).Times(1);
+  gate.set_value();
+
+  future1.get();
+  future2.get();
+
+  EXPECT_EQ(counter, 3) << "The wrong number of callbacks were issued";
+
+  // What happens after is out of scope for this test
+  EXPECT_CALL(*customizer, RemovedBundle).Times(::testing::AnyNumber());
+  bundleTracker->Close();
 }
+#endif

c25c2a13d3cce6a8367dea26517a174175886f60
Jeff DiClemente <jeffdiclemente@users.noreply.github.com>
2023-02-15 08:03:30 -0500

    Fix race with DS service object construction (#801)
    * Fix race with DS service object construction
    
    A race occurs when DS is constructing a service object with injectable mandatory service dependencies whereby any bound service dependencies could no longer be available because another thread unregistered any of the bound service dependencies.
    
    Given that a ComponentContext is constructed only after all the service dependencies has been satisfied, if during ComponetContext construction any of the bound service dependencies are no nullptrs then throw and fail to construct the service object. This fixes a failure mode where a service constructor can be called with a nullptr service object when the service dependency is defined as mandatory and injection is true.
    
    Also noticed that one of the test bundle project was exceeding the path limit on Windows, so shortened the project name to mitigate that problem.
    
    Signed-off-by: The MathWorks, Inc. <jdicleme@mathworks.com>
    
    * Add more tests
    
    Added more tests and fixed up some error messaging
    
    Signed-off-by: The MathWorks, Inc. <jdicleme@mathworks.com>
    
    ---------
    
    Signed-off-by: The MathWorks, Inc. <jdicleme@mathworks.com>

--- compendium/DeclarativeServices/src/manager/SingletonComponentConfiguration.cpp
+++ compendium/DeclarativeServices/src/manager/SingletonComponentConfiguration.cpp
@@ -216,7 +216,7 @@ namespace cppmicroservices
             if (!context->AddToBoundServicesCache(refName, ref))
             {
                 GetLogger()->Log(cppmicroservices::logservice::SeverityLevel::LOG_WARNING,
-                                 "Failure while trying to add reference to BoundServices Cache ");
+                                 "Failure while adding reference " + refName + " to the bound services cache.");
                 return;
             }
             try
@@ -226,8 +226,8 @@ namespace cppmicroservices
             catch (std::exception const&)
             {
                 GetLogger()->Log(cppmicroservices::logservice::SeverityLevel::LOG_ERROR,
-                                 "Exception received from user code while binding a "
-                                 "service reference.",
+                                 "Exception received from user code while binding "
+                                 "service reference" + refName + ".",
                                  std::current_exception());
             }
         }
@@ -243,8 +243,8 @@ namespace cppmicroservices
             catch (std::exception const&)
             {
                 GetLogger()->Log(cppmicroservices::logservice::SeverityLevel::LOG_ERROR,
-                                 "Exception received from user code while unbinding a "
-                                 "service reference.",
+                                 "Exception received from user code while unbinding "
+                                 "service reference" + refName + ".",
                                  std::current_exception());
             }
             auto context = GetComponentContext();

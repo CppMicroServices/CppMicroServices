aaed0ebc4b2d1c04cb070480354840ae4b3fc7d2
tcormack <tcormack@mathworks.com>
2023-07-11 12:33:37 -0400

    add sleep to stop spin

--- framework/test/bundles/libC1/TestBundleC1.cpp
+++ framework/test/bundles/libC1/TestBundleC1.cpp
@@ -132,6 +132,8 @@ namespace cppmicroservices
                         count--;
                     }
                 }
+                // stop spin
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
         }
 

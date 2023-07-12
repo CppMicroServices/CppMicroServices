abeda67412b0c797a245b555f340a8ab9e4f54f1
tcormack <tcormack@mathworks.com>
2023-07-12 11:12:53 -0400

    fixed tracker race

--- framework/include/cppmicroservices/detail/ServiceTracker.hpp
+++ framework/include/cppmicroservices/detail/ServiceTracker.hpp
@@ -139,8 +139,16 @@ void ServiceTracker<S, T>::Open()
 template<class S, class T>
 void ServiceTracker<S, T>::Close()
 {
+  // Copy required to prevent data race in call to context.RemoveListener()
+  ListenerToken tokenCpy;
+  {
+    auto l = d->Lock();
+    US_UNUSED(l);
+    std::swap(d->listenerToken, tokenCpy);
+  }
+
   try {
-    d->context.RemoveListener(std::move(d->listenerToken));
+    d->context.RemoveListener(std::move(tokenCpy));
   } catch (const std::runtime_error& /*e*/) {
     /* In case the context was stopped or invalid. */
   }

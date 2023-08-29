cc03bc00201c26595c49b826a3926675e6796023
Ingmar Sittl <79453136+insi-eb@users.noreply.github.com>
2023-08-29 13:57:01 +0200

    Ensure multiple listeners for the same factory PID are honoured by `ConfigurationNotifier::AnyListenersForPid` (#865) (#904)
    * Ensure multiple listeners for the same PID are honoured
    
    
    
    * Fix formatting
    
    
    
    ---------
    
    Signed-off-by: Conor Burgess <Burgess.Conor@gmail.com>
    Co-authored-by: Conor Burgess <Burgess.Conor@gmail.com>
    Co-authored-by: cburgess <cburgess@mathworks.com>

--- compendium/DeclarativeServices/src/manager/ConfigurationNotifier.cpp
+++ compendium/DeclarativeServices/src/manager/ConfigurationNotifier.cpp
@@ -116,7 +116,7 @@ namespace cppmicroservices
         ConfigurationNotifier::AnyListenersForPid(std::string const& pid) noexcept
         {
             std::string factoryName;
-            std::shared_ptr<ComponentConfigurationImpl> mgr;
+            std::vector<std::shared_ptr<ComponentConfigurationImpl>> mgrs;
             {
                 auto listenersMapHandle = listenersMap.lock();
                 if (listenersMapHandle->empty() || pid.empty())
@@ -148,16 +148,27 @@ namespace cppmicroservices
                 {
                     return false;
                 }
-                auto listener = iter->second->begin();
-
-                mgr = listener->second.mgr;
-                if (mgr->GetMetadata()->factoryComponentID.empty())
+                auto tokenMapPtr = iter->second;
+                for (auto const& tokenEntry : (*tokenMapPtr))
                 {
-                    // The component in our listener's map is not a factory component.
+                    auto listener = tokenEntry.second;
+
+                    if (!listener.mgr->GetMetadata()->factoryComponentID.empty())
+                    {
+                        // The component in our listeners map is a factory component.
+                        mgrs.emplace_back(listener.mgr);
+                    }
+                }
+                if (mgrs.empty())
+                {
+                    // None of the components in our listeners map is a factory component.
                     return false;
                 }
             } // release listenersMapHandle lock
-            CreateFactoryComponent(pid, mgr);
+            for (auto & mgr : mgrs)
+            {
+                CreateFactoryComponent(pid, mgr);
+            }
             return true;
         }
         void

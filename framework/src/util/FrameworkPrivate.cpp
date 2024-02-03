fb6628aefd1872652a2234de2c64f786a5eb8f32
tcormackMW <113473781+tcormackMW@users.noreply.github.com>
2024-02-02 19:47:40 -0500

    Bundle Start after Framework Stop race fix (#990)

--- framework/src/util/FrameworkPrivate.cpp
+++ framework/src/util/FrameworkPrivate.cpp
@@ -146,7 +146,6 @@ namespace cppmicroservices
     {
         auto l = Lock();
         US_UNUSED(l);
-        auto writerLock = coreCtx->SetFrameworkStateAndBlockUntilComplete(true);
         bool wasActive = false;
         switch (static_cast<Bundle::State>(state.load()))
         {
@@ -291,7 +290,10 @@ namespace cppmicroservices
             {
                 StopAllBundles();
             }
-            coreCtx->Uninit0();
+            {
+                auto lock = coreCtx->SetFrameworkStateAndBlockUntilComplete(true);
+                coreCtx->Uninit0();
+            }
             {
                 auto l = Lock();
                 US_UNUSED(l);

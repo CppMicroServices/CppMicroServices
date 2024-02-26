1a69aa3923c6ddc41622d8eb50bebefeaa6fb92b
pelliott-mathworks <67922241+pelliott-mathworks@users.noreply.github.com>
2024-02-26 15:28:33 -0500

    Update ServiceImpl.hpp
    Updating link tidy warnings. <pelliott@mathworks.com>

--- compendium/test_bundles/TestBundleDSFAC1/src/ServiceImpl.hpp
+++ compendium/test_bundles/TestBundleDSFAC1/src/ServiceImpl.hpp
@@ -1,5 +1,5 @@
-#ifndef _SERVICE_IMPL_HPP_
-#define _SERVICE_IMPL_HPP_
+#ifndef SERVICE_IMPL_HPP_
+#define SERVICE_IMPL_HPP_
 
 #include <TestInterfaces/Interfaces.hpp>
 #include "cppmicroservices/servicecomponent/ComponentContext.hpp"
@@ -32,4 +32,4 @@ namespace sample
     };
 } // namespace sample
 
-#endif // _SERVICE_IMPL_HPP_
+#endif // SERVICE_IMPL_HPP_

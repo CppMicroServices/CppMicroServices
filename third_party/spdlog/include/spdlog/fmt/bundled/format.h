f7b36055b1a2a3b207aabf65c68777e7296c0a96
tcormack <tcormack@mathworks.com>
2024-02-26 11:12:55 -0500

    silence warnings

--- third_party/spdlog/include/spdlog/fmt/bundled/format.h
+++ third_party/spdlog/include/spdlog/fmt/bundled/format.h
@@ -485,10 +485,9 @@ inline auto get_data(Container& c) -> typename Container::value_type* {
   return c.data();
 }
 
-#if defined(_SECURE_SCL) && _SECURE_SCL &&  __cpp_lib_span >= 202002L
-#include <span>
+#if defined(_SECURE_SCL) && _SECURE_SCL
 // Make a checked iterator to avoid MSVC warnings.
-template <typename T> using checked_ptr = std::span<T*>;
+template <typename T> using checked_ptr = stdext::checked_array_iterator<T*>;
 template <typename T>
 constexpr auto make_checked(T* p, size_t size) -> checked_ptr<T> {
   return {p, size};

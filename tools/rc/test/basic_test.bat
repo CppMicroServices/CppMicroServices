1dd78b1e7baf71f55e95937447a20dcf16823b20
Michael Carney <mike@carneyweb.com>
2024-03-28 12:05:42 -0400

    deterministic testing

---
+++ tools/rc/test/basic_test.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Cleanup
+del empty.txt
+del first.zip
+del second.zip
+
+:: Create an empty text file
+echo. > empty.txt
+
+:: Zip the file
+call "%RC_EXE%" --out-file test1.zip --res-add test_file.txt --bundle-name test_bundle
+:: Sleep for 2 seconds
+timeout /t 2 >nul
+:: Zip the file again
+call "%RC_EXE%" --out-file test2.zip --res-add test_file.txt --bundle-name test_bundle
+
+:: Calculate SHA256 checksums for the zip files
+for /f "tokens=*" %%a in ('certutil -hashfile "first.zip" SHA256 ^| find /v "CertUtil"') do set "hash1=%%a"
+for /f "tokens=*" %%b in ('certutil -hashfile "second.zip" SHA256 ^| find /v "CertUtil"') do set "hash2=%%b"
+
+:: Compare checksums
+if NOT "!hash1!"=="!hash2!" (
+    echo ERROR: The zip files have DIFFERENT SHA256 checksums.
+    exit /b 1
+)
+
+endlocal
--- tools/rc/test/basic_test.sh
+++ tools/rc/test/basic_test.sh
@@ -8,8 +8,8 @@ $RC_EXE --out-file test1.zip --res-add test_file.txt --bundle-name test_bundle
 sleep 2
 $RC_EXE --out-file test2.zip --res-add test_file.txt --bundle-name test_bundle
 
-test1_shasum=`$SHA_SUM_CMD test1.zip | awk '{print $1}'`
-test2_shasum=`$SHA_SUM_CMD test2.zip | awk '{print $1}'`
+test1_shasum=`shasum -b -a 512 test1.zip | awk '{print $1}'`
+test2_shasum=`shasum -b -a 512 test2.zip | awk '{print $1}'`
 
 ([ -f test1.zip ] && [ -f test2.zip ] && [ $test1_shasum == $test2_shasum ] && exit 0) \
     || (echo "sha 512 chechsums do not match eventhough deterministic bundle builds are enabled" && exit 1)

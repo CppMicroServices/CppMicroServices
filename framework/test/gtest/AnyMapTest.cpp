5748fbb01f1e79c25ed9123377d574d44bce48eb
tcormack <tcormack@mathworks.com>
2024-06-11 14:02:52 -0400

    fix for formatting

--- framework/test/gtest/AnyMapTest.cpp
+++ framework/test/gtest/AnyMapTest.cpp
@@ -443,19 +443,15 @@ TEST(AnyMapTest, InitializerList)
     EXPECT_EQ(Any(7), unordered_any_cimap.at("g"));
     EXPECT_EQ(Any(8), unordered_any_cimap.at("h"));
     AnyMap nested {
-        any_map::UNORDERED_MAP_CASEINSENSITIVE_KEYS,
         {{ "n", 9 }, { "o", 10 }, { "p", std::string("C") }}
     };
     AnyMap unordered_any_nested {
-        any_map::UNORDERED_MAP_CASEINSENSITIVE_KEYS,
         {
-                                              { "j", 7 },
-                                              { "k", 8 },
-                                              { "l", std::string("B") },
-                                              { "m",
-                                              AnyMap(AnyMap { any_map::UNORDERED_MAP_CASEINSENSITIVE_KEYS,
-                                              { { "n", 9 }, { "o", 10 }, { "p", std::string("C") } } }) },
-                                              }
+         { "j", 7 },
+         { "k", 8 },
+         { "l", std::string("B") },
+         { "m", AnyMap { { { "n", 9 }, { "o", 10 }, { "p", std::string("C") } } } },
+         }
     };
     EXPECT_EQ(any_map::UNORDERED_MAP_CASEINSENSITIVE_KEYS, unordered_any_nested.GetType());
     EXPECT_EQ(4, unordered_any_nested.size());

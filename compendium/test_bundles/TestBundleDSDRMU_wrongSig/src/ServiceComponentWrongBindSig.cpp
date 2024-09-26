0a46dd3459c2d5cdf6cebc62b33aeae4357eafae
Toby Cormack <tcormack@vdi-ah2ddp-086.dhcp.mathworks.com>
2024-09-26 11:52:46 -0400

    a couple approaches here

---
+++ compendium/test_bundles/TestBundleDSDRMU_wrongSig/src/ServiceComponentWrongBindSig.cpp
@@ -0,0 +1,58 @@
+#include "ServiceImpl.hpp"
+#include <iostream>
+
+namespace sample
+{
+
+    std::string
+    ServiceComponentWrongBindSig::ExtendedDescription()
+    {
+        std::lock_guard<std::mutex> lock(fooMutex);
+        if (!foo)
+        {
+            throw std::runtime_error("Dependency not available");
+        }
+        std::string result("ServiceComponentWrongBindSig ");
+        result.append("depends on ");
+        result.append(foo->Description());
+        return result;
+    }
+    // ServiceComponentWrongBindSig::Bindfoo(std::shared_ptr<test::Interface1> const& theFoo, bool /*someBool*/)
+    // ServiceComponentWrongBindSig::Bindfoo(std::shared_ptr<test::Interface1> const& theFoo)
+
+    void
+    ServiceComponentWrongBindSig::Bindfoo(std::shared_ptr<test::Interface1> const& theFoo)
+    {
+        std::lock_guard<std::mutex> lock(fooMutex);
+        if (foo != theFoo)
+        {
+            foo = theFoo;
+        }
+    }
+    // ServiceComponentWrongBindSig::Unbindfoo(std::shared_ptr<test::Interface1> const& theFoo, bool /*someBool*/)
+    // ServiceComponentWrongBindSig::Unbindfoo(std::shared_ptr<test::Interface1> const& theFoo)
+
+    void
+    ServiceComponentWrongBindSig::Unbindfoo(std::shared_ptr<test::Interface1> const& theFoo)
+    {
+        std::lock_guard<std::mutex> lock(fooMutex);
+        if (foo == theFoo)
+        {
+            foo = nullptr;
+        }
+    }
+    // void
+    // ServiceComponentWrongBindSig::Modified(std::shared_ptr<ComponentContext> const& /*context*/,
+    //                                        std::shared_ptr<cppmicroservices::AnyMap> const& /*configuration*/)
+    // {
+    // }
+    // void
+    // ServiceComponentWrongBindSig::Activate(std::shared_ptr<ComponentContext> const& /*context*/)
+    // {
+    // }
+    // void
+    // ServiceComponentWrongBindSig::Deactivate(std::shared_ptr<ComponentContext> const& /*context*/)
+    // {
+    // }
+
+} // namespace sample
---
+++ compendium/test_bundles/TestBundleDSDRMU_wrongSig/src/ServiceImpl.hpp
@@ -0,0 +1,40 @@
+#ifndef _SERVICE_IMPL_HPP_
+#define _SERVICE_IMPL_HPP_
+
+#include <mutex>
+
+#include "TestInterfaces/Interfaces.hpp"
+#include "cppmicroservices/servicecomponent/ComponentContext.hpp"
+#include "cppmicroservices/servicecomponent/ComponentInstanceAssertion.hpp"
+using ComponentContext = cppmicroservices::service::component::ComponentContext;
+
+namespace sample
+{
+
+    class ServiceComponentWrongBindSig final
+        : public test::Interface2
+        , cppmicroservices::service::component::ComponentInstanceAssertion
+    {
+      public:
+        ServiceComponentWrongBindSig() = default;
+        ~ServiceComponentWrongBindSig() = default;
+        std::string ExtendedDescription() override;
+
+        void Bindfoo(std::shared_ptr<test::Interface1> const& theFoo);
+        void Unbindfoo(std::shared_ptr<test::Interface1> const& theFoo);
+
+        // void Bindfoo(std::shared_ptr<test::Interface1> const& theFoo, bool /*someBool*/);
+        // void Unbindfoo(std::shared_ptr<test::Interface1> const& theFoo, bool /*someBool*/);
+        void Modified(std::shared_ptr<ComponentContext> const& context,
+                      std::shared_ptr<cppmicroservices::AnyMap> const& configuration);
+        void Activate(std::shared_ptr<ComponentContext> const& context);
+        void Deactivate(std::shared_ptr<ComponentContext> const& context);
+
+      private:
+        std::shared_ptr<test::Interface1> foo;
+        std::mutex fooMutex;
+    };
+
+} // namespace sample
+
+#endif // _SERVICE_IMPL_HPP_

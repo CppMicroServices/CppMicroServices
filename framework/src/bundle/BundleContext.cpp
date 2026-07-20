9e1c749fb86de05b24f136911035e1f84ed6dfb5
Toby Cormack <tcormack@d13-dev.dhcp.mathworks.com>
2026-07-20 16:03:19 -0400

    revert reversion

--- framework/src/bundle/BundleContext.cpp
+++ framework/src/bundle/BundleContext.cpp
@@ -243,8 +243,7 @@ namespace cppmicroservices
     std::shared_ptr<void>
     BundleContext::GetService(ServiceReferenceBase const& reference)
     {
-        // if (!reference.d.Load()->coreInfo)
-        if (!reference)
+        if (!reference.d.Load()->coreInfo)
         {
             throw std::invalid_argument("Default constructed ServiceReference is not a "
                                         "valid input to GetService()");
@@ -265,8 +264,7 @@ namespace cppmicroservices
     InterfaceMapConstPtr
     BundleContext::GetService(ServiceReferenceU const& reference)
     {
-        // if (!reference.d.Load()->coreInfo)
-        if (!reference)
+        if (!reference.d.Load()->coreInfo)
         {
             throw std::invalid_argument("Default constructed ServiceReference is not a "
                                         "valid input to GetService()");

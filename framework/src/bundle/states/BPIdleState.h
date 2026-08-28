f7de8cda47538b95b14046e635dfc7c29f362186
Howell Xia <howellx21@gmail.com>
2026-08-28 09:08:52 -0400

    new function syntax error fix

--- framework/src/bundle/states/BPIdleState.h
+++ framework/src/bundle/states/BPIdleState.h
@@ -11,7 +11,7 @@ namespace cppmicroservices
         public:
         using BundlePrivateState::BundlePrivateState;
         void StartFromIdle(BundlePrivate&, std::shared_ptr<BPActiveState> const&);
-        void BPIdleState::StartFailed(BundlePrivate&);
+        void StartFailed(BundlePrivate&);
         
     };
 } 
